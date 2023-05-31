0f90e403f446bf9cda5745367121a43a51184148
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-31 15:52:26 +0200

    Song recommendations

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 import spotipy
+import openai
 from flask import request, redirect, url_for, session
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
 import os
@@ -14,6 +16,13 @@ client_id = os.getenv("SPOTIPY_CLIENT_ID")
 client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
 redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
 username = os.getenv("SPOTIPY_USER_NAME")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_api_base_url = os.getenv("OPENAI_API_BASE_URL")
+
+# Set the OpenAI API key and base URL
+openai.api_key = openai_api_key
+openai.api_base = openai_api_base_url
+
 
 # Create a Spotify OAuth object
 sp_oauth = SpotifyOAuth(
@@ -24,7 +33,54 @@ sp_oauth = SpotifyOAuth(
     username=username,
 )
 
+"""
+Generate song suggestions based on a seed song.
 
+:param seed_song: The seed song to generate suggestions from.
+:type seed_song: str
+:param num_suggestions: The number of song suggestions to generate, defaults to 10.
+:type num_suggestions: int
+:return: A list of song suggestions based on the seed song.
+:rtype: List[str]
+"""
+def generate_song_suggestions(seed_song, num_suggestions=10):
+    prompt = f"Based on the song '{seed_song}', please suggest {num_suggestions} similar songs."
+
+    response = openai.ChatCompletion.create(
+    model='gpt-4',
+    messages=[
+        {'role': 'user', 'content': prompt},
+    ]
+)
+    
+    print("response: ", response)
+
+    return response
+
+    #suggestions = response.choices[0].text.strip().split("\n")
+    #return suggestions[:num_suggestions]
+
+
+"""
+Defines a POST endpoint that generates song suggestions based on a given seed song.
+
+Returns:
+ A JSON object containing a list of suggested songs based on the seed song.
+"""
+@app.route('/generate_suggestions', methods=['POST'])
+def generate_suggestions():
+    data = request.get_json()
+    seed_song = data.get('song')
+    suggestions = generate_song_suggestions(seed_song)
+    return jsonify(suggestions)
+
+"""
+This function is the callback endpoint for the application's OAuth2 authentication flow. 
+It receives an authorization code from the user's grant, exchanges it for an access token, and stores the token in the user's session. 
+If successful, the user is redirected to the application's index page. 
+If the user does not provide a code or the token cannot be obtained, an error message is returned.
+If successful, redirects the user to the application's index page. If unsuccessful, returns an error message string.
+"""
 @app.route("/callback")
 def callback():
     code = request.args.get("code")
@@ -36,8 +92,26 @@ def callback():
     return "Error: No code provided or token not obtained."
 
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route("/", methods=["GET"])
 def index():
+    return render_template(
+        "index.html",
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+    )
+
+
+"""
+Defines the index route for the web application. Handles GET and POST requests.
+On a POST request, creates a Spotify playlist based on user input and adds recommended songs.
+Returns a redirect to the success page on a successful POST request.
+On a GET request, renders the index.html template with the necessary Spotify credentials.
+:return: A redirect to the success page on a successful POST request. Otherwise, renders the index.html template.
+"""
+@app.route("/generate_playlist", methods=["POST"])
+def create_playlist():
     if request.method == "POST":
         playlist_name = request.form["playlist_name"]
         playlist_description = request.form["playlist_description"]
@@ -76,6 +150,9 @@ def index():
         redirect_uri=redirect_uri,
     )
 
+"""
+Renders the success.html template with the playlist_name parameter if it exists in the current request arguments.
+"""
 @app.route("/success")
 def success():
     playlist_name = request.args.get("playlist_name", "")
